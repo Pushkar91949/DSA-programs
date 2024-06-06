a36c845a2971c39ad31f8342849c19731ebd3e2d
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-06-06 16:17:02 +0530

    Create Check_if_Circular_Linked_List.java

---
+++ Java/Check_if_Circular_Linked_List.java
@@ -0,0 +1,28 @@
+
+/* Structure of LinkedList
+class Node
+{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}
+*/
+class GfG
+{
+    boolean isCircular(Node head)
+    {
+        Node Traveller = head.next;
+        
+        while (Traveller != null){
+            if(Traveller == head){
+                return true;
+            }
+            Traveller = Traveller.next;
+        }
+        return false;
+    }
+} // Question link: https://www.geeksforgeeks.org/problems/circular-linked-list/1
