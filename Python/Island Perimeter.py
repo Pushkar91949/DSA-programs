28d067744acfeef8a4a7bb99b5d52208cf261604
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-07-25 17:29:42 +0530

    Update Island Perimeter.py

--- Python/Island Perimeter.py
+++ Python/Island Perimeter.py
@@ -14,3 +14,4 @@ class Solution:
                     if c>0 and grid[r][c-1] == 1:
                         ans -= 2
         return ans
+# Question link: https://leetcode.com/problems/island-perimeter/
